c88829653a17f8d5ac62041852b3dde743dfe4e7
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2023-04-26 18:28:36 -0400

    feature: comments added

--- game_driver.py
+++ game_driver.py
@@ -2,7 +2,6 @@ from board import display_moves, game_over, dice_roll
 import moves
 import property
 
-
 # Statically instantiating three players
 player1 = property.Player('Ali', 0, 0, 1500, [], False, False)
 player2 = property.Player('Poorvi', 0, 0, 1500, [], False, False)
@@ -22,16 +21,22 @@ incorrect_move = True
 display_moves()
 while not is_game_over:
     for player in player_list:
-            print(f'{player.username}\'s turn ')
-            print(f'Cash - {player.balance}\t Rounds played - {player.game_round}\t Player position - {player.position}')
-            print('----------------------------------------------------------------')
-            while incorrect_move:
-                game_input = input('Enter your choice : ')
-                print(end='\n\n')
-                if game_input.casefold() == 'r':
-                    moves.player_moves(player, dice_roll(player))
-                    incorrect_move = False
-                else:
-                    print('Error! Incorrect Input', end='\n\n')
-                    incorrect_move = True
-            is_game_over = game_over(player)
+
+        # Displays player details
+        print(f'{player.username}\'s turn ')
+        print(f'Cash - {player.balance}\t Rounds played - {player.game_round}\t Player position - {player.position}')
+        print('----------------------------------------------------------------')
+
+        # Move validation
+        while incorrect_move:
+            game_input = input('Enter your choice : ')
+            print(end='\n\n')
+            if game_input.casefold() == 'r':
+                moves.player_moves(player, dice_roll(player))
+                incorrect_move = False
+            else:
+                print('Error! Incorrect Input', end='\n\n')
+                incorrect_move = True
+
+        # Game over condition
+        is_game_over = game_over(player)
