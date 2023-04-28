f2a7ae668c7fa37dfc98c83b60a2637647c79f86
poorvilakkadi <129799980+poorvilakkadi@users.noreply.github.com>
2023-04-28 14:48:00 -0400

    check this and change

--- moves.py
+++ moves.py
@@ -17,25 +17,35 @@ def player_moves(player, dice_value):
         # print(player.position)
         print(f'You are now at {board.BOARD_TILES[player.position]}')
         print(f'{ board.BOARD_TILES[player.position]} Details {board.BOARD_TILES_INFO[board.BOARD_TILES[player.position]]}', end='\n\n\n')
+        owned = board.BOARD_TILES_INFO[board.BOARD_TILES[player.position]][7]
+        if owned == "False" :
+                price= board.BOARD_TILES_INFO[board.BOARD_TILES[player.position]][2]
+                if player.balance >= price:
+                        player.balance -= price
+                        player.assets_owned.append(board.BOARD_TILES[player.position])
+                        board.BOARD_TILES_INFO['owned'] = True
+                        print(f"{player.username} bought {board.BOARD_TILES[player.position]} for {price} rupees.")
+                else:
+                        print(f"{player.username} has insufficient funds.")
+        else: 
+                print(f"{board.BOARD_TILES[player.position]} is already owned")
+        
 
 
 # player_moves(player1, board.dice_roll())
 # player_moves(player2, board.dice_roll())
 # player_moves(player3, board.dice_roll())
 
-class prop:
-    def __init__(self, price, name):
-        self.name = name
-        self.owner = None
-        self.price = price
-
-    def buy(self, player):
-        if self.owner is None:
-            if player.money >= self.price:
-                player.money -= self.price
-                self.owner = player
-                print(f"{player.name} bought {self.name} for {self.price} dollars.")
-            else:
-                print(f"{player.name} does not have enough money to buy {self.name}.")
-        else:
-            print(f"{self.name} is already owned by {self.owner.name}.")
+##def buy(player):
+      #if player.assets_owned is None:
+        #price= board.BOARD_TILES_INFO[board.BOARD_TILES[player.position]][2]
+        #
+        #1print(price)
+        #if player.balance >= : 
+                #player.money -= self.price
+                #self.owner = player
+                #print(f"{player.username} bought {self.name} for {self.price} dollars.")
+        #else:
+#               print(f"{player.name} does not have enough money to buy {self.name}.")
+#else: 
+#print(f"{self.name} is already owned by {self.owner.name}.")
