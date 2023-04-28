d2045562acb5f5d3d2c018abfc46a94386d26890
poorvilakkadi <129799980+poorvilakkadi@users.noreply.github.com>
2023-04-28 13:20:51 -0400

    just trial buy

--- moves.py
+++ moves.py
@@ -23,3 +23,19 @@ def player_moves(player, dice_value):
 # player_moves(player2, board.dice_roll())
 # player_moves(player3, board.dice_roll())
 
+class prop:
+    def __init__(self, price, name):
+        self.name = name
+        self.owner = None
+        self.price = price
+
+    def buy(self, player):
+        if self.owner is None:
+            if player.money >= self.price:
+                player.money -= self.price
+                self.owner = player
+                print(f"{player.name} bought {self.name} for {self.price} dollars.")
+            else:
+                print(f"{player.name} does not have enough money to buy {self.name}.")
+        else:
+            print(f"{self.name} is already owned by {self.owner.name}.")
