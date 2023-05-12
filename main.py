c747bafdb8b3848915e478c8a99814f5f341b3f5
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2023-05-12 05:21:01 -0400

    feature: user api stuff

--- main.py
+++ main.py
@@ -1,19 +1,79 @@
 # FastAPI import statements
-from fastapi import FastAPI, Request, status
+from uuid import UUID, uuid4
 from pydantic import BaseModel
+from fastapi import FastAPI, Request, Response, status, Depends
+from fastapi_sessions.backends.implementations import InMemoryBackend
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
-#from move import router as MoveRouter
+# from move import router as MoveRouter
 from fastapi.templating import Jinja2Templates
-from database.Dao import Dao 
-from database.DaoConstants import DaoConstants 
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+from database.Dao import Dao
+from database.DaoConstants import DaoConstants
 from service.userHandle import UserHandle
 from models.user import User
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse 
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.responses import JSONResponse
+
+from sessions.auth import encode_password, verify_password
+from sessions.base_verifier import BasicVerifier
+from sessions.core_types import SessionData
+
 # Game Logic import statements
 
 app = FastAPI()
+cookie_params = CookieParameters()
+backend = InMemoryBackend[UUID, SessionData]()
+
+cookie = SessionCookie(
+    cookie_name="cookie",
+    identifier="general_verifier",
+    auto_error=True,
+    secret_key="DONOTUSE",
+    cookie_params=cookie_params,
+)
+
+verifier = BasicVerifier(
+    identifier="general_verifier",
+    auto_error=True,
+    backend=backend,
+    auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
+)
+
+
+class UserRegisterIn(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class UserLoginIn(BaseModel):
+    email: str
+    password: str
+
+
+@app.get("/whoami", dependencies=[Depends(cookie)])
+async def whoami(session_data: SessionData = Depends(verifier)):
+    return session_data
+
+
+@app.post("/delete_session")
+async def del_session(response: Response, session_id: UUID = Depends(cookie)):
+    await backend.delete(session_id)
+    cookie.delete_from_response(response)
+    return "deleted session"
+
+
+@app.post("/create_session/{name}")
+async def create_session(name: str, response: Response):
+    session = uuid4()
+    data = SessionData(username=name)
+
+    await backend.create(session, data)
+    cookie.attach_to_response(response, session)
+
+    return f"created session for {name}"
+
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
@@ -22,44 +82,66 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         content={"message": "Internal server error"},
     )
 
+
 @app.get("/getLogs", response_class=JSONResponse)
 async def get_logs(request: Request, room_id: int):
     try:
-        db=Dao()
-        daoConst=DaoConstants()
+        db = Dao()
+        daoConst = DaoConstants()
         logs = await db.select_query(daoConst.SELECT_LOGS_BY_ROOM_ID, (room_id,))
         log_messages = [log[0] for log in logs]
         return "\n".join(log_messages)
     except Exception as e:
         raise Exception("Error in getting logs")
-    
 
-@app.get("/register", response_class=JSONResponse)
-async def register(request: Request, user_name: str,emailId:str,pwd:str):
-    try:
-       userrr=User( user_id=None,username=user_name,email=emailId, password=pwd)
-       u=UserHandle()
-       isSuccessfull=await u.createUser(userrr)
-       if isSuccessfull:
-        return "User Created"
-       else:
-        return("Unable to register")
-    except Exception as e:
-        return("Unable to register")
-        #raise Exception("Unable to register")
-    
-@app.post("/login", response_class=JSONResponse)
-async def login(request: Request,emailId:str,pwd:str):
-    try:
-       if emailId or pwd:
-            print('Recieved Input',emailId,' , pwd= ',pwd)
-            userrr=User( user_id=None,username=None,email=emailId, password=pwd)
-            u=UserHandle()
-            username= u.loginUser(userrr)
-            if username:
-                    value="Welcome back ",username
-                    return value
-       else:
-        return "unable to login"
-    except Exception as e:
-        raise Exception("Unable to login")
+
+@app.post("/register")
+async def register(user_input: UserRegisterIn):
+    dao = Dao()
+    _check = dao.insertion_query(DaoConstants.CREATE_USER, (user_input.username,
+                                                   user_input.email,
+                                                   encode_password(user_input.password)))
+    print(_check)
+    return {'message': 'success'}, 200
+
+
+# @app.post("/login")
+# async def login(user: UserLoginIn, response: Response):
+#     dao = Dao()
+#     _user = dao.select_query(DaoConstants.GET_USER, ('test@example.com',))
+#
+#     username = "something1"
+#     password = "someting___random"
+#
+#     session = uuid4()
+#     data = SessionData(username=username)
+#     await backend.create(session, data)
+#     cookie.attach_to_response(response, session)
+#
+#     # check = safe_str_cmp(_user[0][1], user.password)
+#     # print(check)
+#     return {}, status.HTTP_200_OK
+
+@app.post("/login")
+async def login(user: UserLoginIn, response: Response):
+    dao = Dao()
+    _user = dao.select_query(DaoConstants.GET_USER, ('hihi@gmail.com',))
+
+    # print(_user)
+
+    # Assuming _user[0][0] is the username and it's stored as a string
+    username = _user[0][0]
+    # Assuming _user[0][1] is the hashed password
+    stored_password = _user[0][1]
+
+    # print(verify_password(stored_password, user.password))
+
+    if verify_password(stored_password, user.password):
+        session = uuid4()
+        data = SessionData(username=username)
+        await backend.create(session, data)
+        cookie.attach_to_response(response, session)
+        return {}, status.HTTP_200_OK
+    else:
+        return {"error": "Invalid login credentials"}, status.HTTP_401_UNAUTHORIZED
+
--- service/userHandle.py
+++ service/userHandle.py
@@ -2,53 +2,48 @@ from models.user import User as u
 import database.Dao as databaseObj
 import database.DaoConstants as DaoConst
 from utils.loging import log
-import bcrypt
+import hashlib
+
 
 class UserHandle:
     def __init__(self):
-        self.db=databaseObj.Dao()
-        self.daoConst=DaoConst.DaoConstants()
-        self.logger=log()
+        self.db = databaseObj.Dao()
+        self.daoConst = DaoConst.DaoConstants()
+        self.logger = log()
 
-    def encodePass(self,password):
-         encoded_pass=password.encode('utf-8')
-         salt = bcrypt.gensalt(rounds=10)
-         hashed_password = bcrypt.hashpw(encoded_pass, salt)
-         hex_pass=hashed_password.hex()
-         return hex_pass
-         
-    #Register working
-    def createUser(self,u):
+    def encodePass(self, password):
+        encoded_pass = password.encode('utf-8')
+        hashed_password = hashlib.sha256(encoded_pass).hexdigest()
+        return hashed_password
+
+    # Register working
+    def createUser(self, u):
         try:
-            alreadyExists=False
-            alreadyExists=self.db.select_query(self.daoConst.GET_EXISTING_USER,(u.username,u.email))
+            alreadyExists = False
+            alreadyExists = self.db.select_query(self.daoConst.GET_EXISTING_USER, (u.username, u.email))
             if not alreadyExists:
-                hex_pass=self.encodePass(u.password)
-                self.db.insertion_query(self.daoConst.CREATE_USER,(u.username,u.email,hex_pass))
+                hex_pass = self.encodePass(u.password)
+                self.db.insertion_query(self.daoConst.CREATE_USER, (u.username, u.email, hex_pass))
                 return True
             else:
-                 print("User already exists")
-                 return False
+                print("User already exists")
+                return False
         except Exception as e:
             print(e)
-            log.log_error(log,message="Unable to create user")  
+            log.log_error(log, message="Unable to create user")
             return False
 
-    #Login
-    def loginUser(self,u):
-            values =self.db.select_query(self.daoConst.GET_USER,(u.email))
-            print('Mah values',values)
-            my_tuple = values[0]
-            username = my_tuple[0]
-            stored_password = my_tuple[1]
-            print('Stored pass is ',stored_password)
-            stored_password=bytes(stored_password)
-            if stored_password is not None and bcrypt.checkpw( u.password.encode('utf-8'), stored_password):
-                print('Passwords Match')
-                return username
-            else:
-                print('Passwords do not match wa wa wa')
-                return None
-            
-
- 
+    # Login
+    def loginUser(self, u):
+        values = self.db.select_query(self.daoConst.GET_USER, (u.email))
+        print('Mah values', values)
+        my_tuple = values[0]
+        username = my_tuple[0]
+        stored_password = my_tuple[1]
+        print('Stored pass is ', stored_password)
+        if stored_password is not None and self.encodePass(u.password) == stored_password:
+            print('Passwords Match')
+            return username
+        else:
+            print('Passwords do not match wa wa wa')
+            return None
